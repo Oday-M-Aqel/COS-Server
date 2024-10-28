b14c23438a32d28f23c1fe3ac7b632c33c9bc552
OdayAqel <odayaqel61@gmail.com>
2024-10-28 23:28:44 +0200

    بشس

--- routes/deleteRoutes.js
+++ routes/deleteRoutes.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const verifyAdmin = require('../middleware/verifyAdminToken');
-const { verifyToken } = require('../middleware/verifyToken');
-const Delete = require('../controller/eventController');
+const verifyAdmin = require("../middleware/verifyAdminToken");
+const { verifyToken } = require("../middleware/verifyToken");
+const Delete = require("../controller/eventController");
 
-router.delete('/doctors/:id', verifyAdmin, Delete.deleteDoctorById);
-router.delete('/patient/:id', verifyToken, Delete.deletePatientById);
-router.delete('/appointment/:id', verifyAdmin, Delete.deleteAppointmentById);
-router.delete('/contact/:id', verifyAdmin, Delete.deleteContactById);
-router.delete('/visit', Delete.deleteVisit);
+router.delete("/doctors/:id", verifyAdmin, Delete.deleteDoctorById);
+router.delete("/patient/:id", verifyToken, Delete.deletePatientById);
+router.delete("/appointment/:id", verifyAdmin, Delete.deleteAppointmentById);
+router.delete("/contact/:id", verifyAdmin, Delete.deleteContactById);
+router.delete("/visit", verifyAdmin, Delete.deleteVisit);
 
 module.exports = router;
